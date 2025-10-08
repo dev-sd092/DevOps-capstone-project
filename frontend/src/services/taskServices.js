015a7b3ec3a2d8f2ffee6202d3fa3a6365b9610c
Sanket Desai <sanketdesai092@gmail.com>
2025-10-08 13:34:04 +0530

    Update taskServices.js

--- frontend/src/services/taskServices.js
+++ frontend/src/services/taskServices.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const apiUrl = process.env.REACT_APP_BACKEND_URL //"http://localhost:8080/api/tasks";
+const apiUrl = 'http://sanketdesai/api/tasks'
 console.log(apiUrl)
 export function getTasks() {
     return axios.get(apiUrl);
@@ -16,3 +16,4 @@ export function updateTask(id, task) {
 export function deleteTask(id) {
     return axios.delete(apiUrl + "/" + id);
 }
+
